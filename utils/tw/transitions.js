944b573cdfdf14662f57b676e157a77da34560d5
Veselin Tsvetanov <veselin.t@actionprgroup.com>
2023-05-10 17:31:19 +0300

    add transition animations

---
+++ utils/tw/transitions.js
@@ -0,0 +1,19 @@
+import { raw, extend } from '$core/utils/i18n';
+
+export const def = {
+    enterActive: 'duration-300 ease-out',
+    enterFrom: 'transform opacity-0',
+    enterTo: 'opacity-100',
+    leaveActive: 'duration-200 ease-in',
+    leaveFrom: 'opacity-100',
+    leaveTo: 'transform opacity-0'
+}
+
+const tweens = {
+
+}
+
+export const tween = (name, extra) => {
+
+};
+

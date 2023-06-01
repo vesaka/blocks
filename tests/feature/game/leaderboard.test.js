274b9893cc67ac6b1096b138065b099eb34a0b60
Veselin Tsvetanov <veselin.t@actionprgroup.com>
2023-06-01 17:37:19 +0300

    added leaderboard page tests

---
+++ tests/feature/game/leaderboard.test.js
@@ -0,0 +1,40 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { t } from '$blocks/tests/utils/i18n.util';
+import { mount } from '@vue/test-utils';
+import Leaderboard from '$blocks/components/pages/Leaderboard.vue';
+import { setActivePinia, createPinia } from 'pinia';
+import { useGameStore } from '$blocks/bootstrap/stores';
+import pinia from '$blocks/bootstrap/pinia';
+import fixtures from '$blocks/tests/fixtures/leaderboard.json';
+
+let store;
+const createWrapper = (data = []) => {
+    setActivePinia(pinia);
+    store = useGameStore();
+    store.players = data;
+    return mount(Leaderboard, {
+        global: {
+            plugins: [pinia]
+        }
+    });
+};
+describe('User', () => {
+    let wrapper;
+
+    it('goes to Leaderboard page and sees the title', () => {
+        wrapper = createWrapper();
+        expect(wrapper.html()).toContain(t('leaderoard.title'));
+    });
+
+    it('sees no results message', () => {
+        wrapper = createWrapper();
+        expect(wrapper.html()).toContain(t('leaderboard.empty'))
+    });
+
+    it('can see top scores', () => {
+        wrapper = createWrapper(fixtures.results);
+        setTimeout(() => {
+            expect(wrapper.html()).not.toContain(t('leaderboard.empty'));
+        }, 300);
+    })
+});

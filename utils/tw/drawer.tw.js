4f411d9212d71fbb83a88b856d7112cf8e36bde2
Veselin <vesakabgr@gmail.com>
2023-06-05 12:53:44 +0300

    added drawer classes

---
+++ utils/tw/drawer.tw.js
@@ -0,0 +1,25 @@
+export const drawerRoot = {
+    'flex flex-col': true,
+    'fixed top-0 left-0 z-40 ': true,
+    'h-screen p-4 overflow-y-auto': true,
+    'bg-white w-80 dark:bg-gray-800': true
+};
+
+export const drawerLabel = {
+    'text-base font-semibold text-gray-500': true,
+    'uppercase dark:text-gray-400': true
+};
+
+export const drawerCloseButton = {
+    'text-gray-400 bg-transparent': true,
+    'hover:bg-gray-200 hover:text-gray-900': true,
+    'rounded-lg text-sm p-1.5': true,
+    'absolute top-2.5 right-2.5': true,
+    'inline-flex items-center': true
+};
+
+export const dimmer = {
+    'fixed top-0 left-0': true,
+    'w-screen h-screen z-20': true,
+    'bg-gray-400 bg-opacity-50': true
+}

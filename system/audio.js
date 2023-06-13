fe1c20fd349c1f0203b35b046df69116cdff2699
Veselin Tsvetanov <veselin.t@actionprgroup.com>
2023-06-13 14:13:33 +0300

    setup sonds

--- system/audio.js
+++ system/audio.js
@@ -1,16 +1,72 @@
 import Container from '$core/container';
-import { watch } from 'vue';
+import { Audio as ThreeAudio, AudioListener } from 'three';
 
 class Audio extends Container {
 
     constructor(options = {}) {
         super(options);
 
+        this.sounds = {};
         this.$listen({
-            'audio': ['on', 'off'],
-            'block': ['', 'released']
+            'audio': ['toggle', 'loaded'],
+            'tak': ['loaded'],
+            'success': ['loaded'],
+            'move': ['end'],
+            'level': ['end']
         });
 
+        this.audio_toggle(this.$store.sound);
+        
+    }
+
+    createBuffer(name, buffer, callback = null) {
+        const audioListener = new AudioListener();
+        this.camera.add(audioListener);
+        
+        this.sounds[name] = new ThreeAudio(audioListener);
+        this.sounds[name].setBuffer(buffer);
+        this.scene.add(this.sounds[name]);
+        
+        if (typeof callback === 'function') {
+            callback(this.sounds[name]);
+        }
+    }
+
+    playBuffer(name) {
+        if (!this.$store.sound) {
+            return;
+        }
+
+        const sound = this.sounds[name];
+        if (sound.isPlaying) {
+            sound.stop();
+        }
+
+       sound.play();
+    }
+
+    tak_loaded(buffer) {
+        this.createBuffer('tak', buffer);
+    }
+
+    success_loaded(buffer) {
+        this.createBuffer('success', buffer);
+    }
+
+    audio_toggle(enable = true) {
+        if(!enable) {
+            for(let name in this.sounds) {
+                this.sounds[name].stop();
+            }
+        }
+    }
+
+    move_end() {
+        this.playBuffer('tak');
+    }
+
+    level_end() {
+        this.playBuffer('success');
     }
 }
 

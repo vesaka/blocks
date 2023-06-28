66bb23b8d5a1579aeea04afb8f896a3f99dcc634
Veselin <vesakabgr@gmail.com>
2023-06-28 08:45:25 +0300

    cleanup

--- bootstrap/paths.js
+++ bootstrap/paths.js
@@ -1,35 +1,12 @@
-import { app } from '$blocks/config/settings';
 import env from './imports';
 
-console.log('env', app);
-export const BASE =  '/' + (env.PROD ? env.VITE_GAME_KEY + '/'  : '');
-export const SIGNUP_PATH = '/sign-up';
-export const LOGIN_PATH = '/log-in';
-export const PLAY_PATH = '/playground';
-export const LEADERBOARD = '/leaderboard';
-export const PAGE_404 = '/404';
-export const FORGOT_PASSWORD = '/forgot-password';
-export const RESET_PASSWORD = '/reset-password';
-export const LEVELS_PATH = '/levels';
-export const SUCCESS_PATH = '/sucess';
-export const COMPLETED = '/completed';
+export const BASE =  env.DEV ? '/' : '/unblockme/';
+export const SIGNUP_PATH = BASE + 'sign-up';
+export const LOGIN_PATH = BASE + 'log-in';
+export const PLAY_PATH = BASE + 'playground';
+export const LEADERBOARD = BASE + 'leaderboard';
+export const PAGE_404 = BASE + '404';
+export const FORGOT_PASSWORD = BASE + 'forgot-password';
+export const RESET_PASSWORD = BASE + 'reset-password';
+export const LEVELS_PATH = BASE + 'levels';
 
-export const svg = path => {
-    return `/assets/svg/${path}.svg`;
-};
-
-export const asset = path => {
-    return `/assets/${path}`;
-};
-
-export const bg = path => {
-    return `/assets/bg/${path}`;
-};
-
-export const btn = path => {
-    return `/assets/btn/${path}.png`;
-};
-
-export const img = path => {
-    return `/assets/img/${path}.png`;
-};
